cd3c7434d1e44445f02d2bff103a12ce5d1ab941
Yeva Stepanova <ys4826@nyu.edu>
2021-09-22 12:39:46 +0400

    recent version
    my project is here!

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* this is small but I put a lot of effort in it to work*/
+/* this is interactivity that asks you to submit your name*/
+
 window.onload = (event) => {
 let result = prompt("Enter Your Name","");
 console.log(document.getElementById("enter-name"))

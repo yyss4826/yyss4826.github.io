6711b70ae7fced56bf14cfaa3f9c6508ac5209b6
Yeva Stepanova <ys4826@nyu.edu>
2021-09-23 01:37:10 +0400

    newest project
    here is my final submission

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/* this is small but I put a lot of effort in it to work*/
-/* this is interactivity that asks you to submit your name*/
-
 window.onload = (event) => {
 let result = prompt("Enter Your Name","");
 console.log(document.getElementById("enter-name"))
 document.getElementById("enter-name").innerText = `Hello, ${result}, this is my movie`;
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click' , () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
 };
 
